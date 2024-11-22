59a5cd00fc38ff3808ecab11e092c62795db104e
Nasir Mollah <nasirmollah192@gmail.com>
2024-11-22 17:23:38 +0600

    updated code.

--- app/dashboard/allblogs/page.tsx
+++ app/dashboard/allblogs/page.tsx
@@ -2,15 +2,221 @@
 
 
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+
+// "use client";
+// import { Button } from "@/components/ui/button";
+// import { Card } from "@/components/ui/card";
+// import { Edit, Eye, Trash2, XCircle } from "lucide-react";
+// import { useDeleteBlogMutation, useGetAllBlogsQuery } from "@/redux/features/api/blogApi";
+// import React, { useState } from "react";
+// import Image from "next/image";
+// import { useAppDispatch } from "@/redux/hooks";
+// import { useRouter } from "next/navigation";
+// import toast from "react-hot-toast";
+// import Loading from "@/components/shared/Loading";
+// import { Article } from "../layout";
+// import { setEditableArticle } from "@/redux/features/slices/blogSlice";
+
+// const AllArticlePage = () => {
+//   const { data, isLoading } = useGetAllBlogsQuery([]);
+//   const [ showPreview, setShowPreview ] = useState<boolean>(false);
+//   const [ selectedArticle, setSelectedArticle ] = useState<Article | null>(null);
+//   const [ deleteArticle ] = useDeleteBlogMutation();
+//   const dispatch = useAppDispatch();
+//   const router = useRouter();
+
+//   if (isLoading) return <Loading />;
+
+//   const articles = data?.data;
+
+//   const handlePreview = (article: Article) => {
+//     setSelectedArticle(article);
+//     setShowPreview(true);
+//   };
+
+//   const handleEditArticle = (article: Article) => {
+//     console.log({ article });
+//     dispatch(setEditableArticle(article));
+//     router.push("/dashboard/addblog");
+//   };
+
+//   const handleDeleteArticle = (id: string) => {
+//     toast(
+//       (t) => (
+//         <div className="flex flex-col items-center space-y-2">
+//           <p>Do you want to delete this article?</p>
+//           <div className="flex space-x-4">
+//             <Button
+//               size="sm"
+//               className="bg-red-500 text-white"
+//               onClick={async () => {
+//                 try {
+//                   const result = await deleteArticle(id);
+//                   if (result.error) {
+//                     toast.error("Failed to delete article!");
+//                   } else if (result?.data?.success) {
+//                     toast.success("Article deleted successfully!");
+//                     toast.dismiss(t.id); // Dismiss the confirmation toast
+//                   }
+//                 } catch {
+//                   toast.error("Failed to delete article!");
+//                 }
+//               }}
+//             >
+//               Yes
+//             </Button>
+//             <Button
+//               size="sm"
+//               variant="outline"
+//               onClick={() => toast.dismiss(t.id)} // Dismiss the confirmation toast
+//             >
+//               No
+//             </Button>
+//           </div>
+//         </div>
+//       ),
+//       {
+//         duration: Infinity, // Keep the toast open until user responds
+//       }
+//     );
+//   };
+
+//   if (!articles?.length) {
+//     return <p>No articles found</p>;
+//   }
+
+//   return (
+//     <div className="space-y-4 w-full">
+//       {articles &&
+//         articles.map((article: Article) => (
+//           <Card
+//             key={article.id}
+//             className="flex flex-col md:flex-row shadow-md rounded-lg overflow-hidden"
+//           >
+//             {/* Article Image or Placeholder */}
+//             <div className="w-full md:w-32 h-48 md:h-32 flex items-center justify-center border-4 border-cyan-600 m-1 rounded-lg p-1 bg-gray-200">
+//               {article.image ? (
+//                 <Image
+//                   height={128} // Predefined size for the image
+//                   width={128}
+//                   src={article.image}
+//                   alt={article.title}
+//                   className="object-cover w-full h-full"
+//                 />
+//               ) : (
+//                 <div className="text-gray-400 text-sm font-medium">Blog</div>
+//               )}
+//             </div>
+
+//             {/* Article Title */}
+//             <div className="p-2 md:p-4 flex-1">
+//               <h3 className="text-lg font-bold text-gray-800">{article.title}</h3>
+//               <div className="flex justify-end space-x-2 mt-4">
+//                 <Button
+//                   variant="outline"
+//                   className="bg-blue-400 hover:bg-blue-500 font-normal md:font-bold text-white"
+//                   size="sm"
+//                   onClick={() => handlePreview(article)}
+//                 >
+//                   <Eye className="mr- h-4 w-4" />
+//                   Preview
+//                 </Button>
+//                 <Button
+//                   variant="outline"
+//                   className="bg-yellow-500 font-normal md:font-bold hover:bg-yellow-600 text-white"
+//                   size="sm"
+//                   onClick={() => handleEditArticle(article)}
+//                 >
+//                   <Edit className="mr- h-4 w-4" />
+//                   Edit
+//                 </Button>
+//                 <Button
+//                   variant="destructive"
+//                   className="font-normal md:font-bold"
+//                   size="sm"
+//                   onClick={() => handleDeleteArticle(article.id)}
+//                 >
+//                   <Trash2 className="mr- h-4 w-4" />
+//                   Delete
+//                 </Button>
+//               </div>
+//             </div>
+//           </Card>
+//         ))}
+
+//       {/* Preview Modal */}
+//       {showPreview && selectedArticle && (
+//         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+//           <div className="bg-white rounded-lg shadow-lg w-full max-w-5xl p-6 relative overflow-auto max-h-[95vh] mx-4">
+//             {/* Close Button */}
+//             <button
+//               className="absolute top-4 right-4 text-white bg-black hover:bg-gray-800 px-2 py-2 rounded-md flex justify-center items-center text-2xl font-bold"
+//               onClick={() => setShowPreview(false)}
+//             >
+//               <XCircle />
+//             </button>
+
+//             {/* Preview Heading */}
+//             <div className="text-center mb-4">
+//               <h3 className="text-black text-3xl font-bold">Preview</h3>
+//             </div>
+
+//             {/* Article Preview Card */}
+//             <div className="mt-8 flex flex-col mx-auto">
+//               {/* Article Title */}
+//               <h4 className="text-center text-4xl font-semibold text-gray-800 mb-4">
+//                 {selectedArticle.title}
+//               </h4>
+
+//               {/* Article Image */}
+//               {selectedArticle.image && (
+//                 <div className="flex justify-center mb-6">
+//                   <Image
+//                     width={700}
+//                     height={700}
+//                     src={selectedArticle.image}
+//                     alt="Selected Article"
+//                     className="w-full max-w-5xl h-auto object-cover rounded-lg shadow-md"
+//                   />
+//                 </div>
+//               )}
+
+//               {/* Article Content */}
+//               <div className="prose max-w-none text-gray-700">
+//                 <div
+//                   dangerouslySetInnerHTML={{
+//                     __html: selectedArticle.content,
+//                   }}
+//                 />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AllArticlePage;
+
+
+
+
+
+
+
+
+
+
 
 "use client";
+import React, { useEffect, useMemo, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { Edit, Eye, Trash2, XCircle } from "lucide-react";
+import { Edit, Eye, Trash2, XCircle, Search } from "lucide-react";
 import { useDeleteBlogMutation, useGetAllBlogsQuery } from "@/redux/features/api/blogApi";
-import React, { useState } from "react";
 import Image from "next/image";
 import { useAppDispatch } from "@/redux/hooks";
 import { useRouter } from "next/navigation";
@@ -18,18 +224,41 @@ import toast from "react-hot-toast";
 import Loading from "@/components/shared/Loading";
 import { Article } from "../layout";
 import { setEditableArticle } from "@/redux/features/slices/blogSlice";
+import { debounce } from "lodash";
 
 const AllArticlePage = () => {
   const { data, isLoading } = useGetAllBlogsQuery([]);
   const [ showPreview, setShowPreview ] = useState<boolean>(false);
   const [ selectedArticle, setSelectedArticle ] = useState<Article | null>(null);
   const [ deleteArticle ] = useDeleteBlogMutation();
+  const [ searchTerm, setSearchTerm ] = useState<string>("");
+  const [ filteredArticles, setFilteredArticles ] = useState<Article[]>([]);
   const dispatch = useAppDispatch();
   const router = useRouter();
 
+
+
+  const articles = useMemo(() => data?.data || [], [ data?.data ]);
+
+  // Initialize filtered articles when articles data is fetched
+  useEffect(() => {
+    setFilteredArticles(articles);
+  }, [articles]);
+
+  const debouncedSearch = debounce((term: string) => {
+    const filtered = articles.filter((article:Article) =>
+      article.title.toLowerCase().includes(term.toLowerCase())
+    );
+    setFilteredArticles(filtered);
+  }, 300);
+
   if (isLoading) return <Loading />;
 
-  const articles = data?.data;
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    debouncedSearch(value);
+  };
 
   const handlePreview = (article: Article) => {
     setSelectedArticle(article);
@@ -37,7 +266,6 @@ const AllArticlePage = () => {
   };
 
   const handleEditArticle = (article: Article) => {
-    console.log({ article });
     dispatch(setEditableArticle(article));
     router.push("/dashboard/addblog");
   };
@@ -83,14 +311,25 @@ const AllArticlePage = () => {
     );
   };
 
-  if (!articles?.length) {
-    return <p>No articles found</p>;
-  }
-
   return (
     <div className="space-y-4 w-full">
-      {articles &&
-        articles.map((article: Article) => (
+      {/* Search Bar */}
+      <div className="flex justify-between w-full items-center mb-4">
+        <div className="relative flex w-full items-center bg-[#000000c5] font-bold text-white border-4 border-cyan-500 px-3 py-2 rounded-lg">
+          <Search className="h-5 w-5 " />
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder="Search by title..."
+            className="bg-transparent outline-none w-full pl-2 "
+          />
+        </div>
+      </div>
+
+      {/* Articles List */}
+      {filteredArticles.length ? (
+        filteredArticles.map((article: Article) => (
           <Card
             key={article.id}
             className="flex flex-col md:flex-row shadow-md rounded-lg overflow-hidden"
@@ -99,14 +338,14 @@ const AllArticlePage = () => {
             <div className="w-full md:w-32 h-48 md:h-32 flex items-center justify-center border-4 border-cyan-600 m-1 rounded-lg p-1 bg-gray-200">
               {article.image ? (
                 <Image
-                  height={128} // Predefined size for the image
+                  height={128}
                   width={128}
                   src={article.image}
                   alt={article.title}
                   className="object-cover w-full h-full"
                 />
               ) : (
-                <div className="text-gray-400 text-sm font-medium">Blog</div>
+                <div className="text-gray-400 text-sm font-medium">No Image</div>
               )}
             </div>
 
@@ -144,7 +383,14 @@ const AllArticlePage = () => {
               </div>
             </div>
           </Card>
-        ))}
+        ))
+      ) : searchTerm ? (
+        // Show "No Results" if search term exists but no articles are found
+        <p className="text-center text-gray-500">No articles match your search.</p>
+      ) : (
+        // Show "No Articles Found" if there are no articles at all
+        <p className="text-center text-gray-500">No articles found.</p>
+      )}
 
       {/* Preview Modal */}
       {showPreview && selectedArticle && (
--- app/dashboard/recommendedUsers/page.tsx
+++ app/dashboard/recommendedUsers/page.tsx
@@ -1,24 +1,191 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-"use client"
-import Image from 'next/image';
-import React, { useState } from 'react';
-import { User } from '../layout';
-import { useGetAllRecommendedUsersQuery, useToggleBlockedMutation, useToggleVerifiedAndRecommendedMutation } from '@/redux/features/api/authApi';
-import Loading from '@/components/shared/Loading';
-import toast from 'react-hot-toast';
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// "use client"
+// import Image from 'next/image';
+// import React, { useState } from 'react';
+// import { User } from '../layout';
+// import { useGetAllRecommendedUsersQuery, useToggleBlockedMutation, useToggleVerifiedAndRecommendedMutation } from '@/redux/features/api/authApi';
+// import Loading from '@/components/shared/Loading';
+// import toast from 'react-hot-toast';
+
+// const RecommendedUserPage = () => {
+//     const { data: allUsersData, isLoading: allUsersDataLoading } = useGetAllRecommendedUsersQuery("");
+//     const [ toggleVerifiedAndRecommended, { isLoading: toggleVerifiedAndRecommendedLoading } ] = useToggleVerifiedAndRecommendedMutation();
+//     const [ toggleBlockedMutation, { isLoading: toggleBlockedLoading } ] = useToggleBlockedMutation();
+//     const [ visibleUsersCount, setVisibleUsersCount ] = useState(20);
+
+//     if (allUsersDataLoading) {
+//         return <Loading />;
+//     }
+
+//     const handleAddRecommended = async (userId: string) => {
+//         const toggleuserStatus = toast.loading("updating user status.")
+//         try {
+//             const response = await toggleVerifiedAndRecommended({ id: userId });
+//             if (response.error) {
+//                 toast.error("Failed to update user status!");
+//                 return;
+//             }
+//             toast.success("User status updated successfully!");
+//         } catch (error) {
+//             toast.error("error while updating user status!")
+//         } finally {
+//             toast.dismiss(toggleuserStatus)
+//         }
+//     };
+
+//     const handleBlockUser = async (userId: string) => {
+//         const toggleuserStatus = toast.loading("updating user status.")
+//         try {
+//             const response = await toggleBlockedMutation({ id: userId });
+//             if (response.error) {
+//                 toast.error("Failed to update user status!");
+//                 return;
+//             }
+//             toast.success("User status updated successfully!");
+//         } catch (error) {
+//             toast.error("error while updating user status!")
+//         } finally {
+//             toast.dismiss(toggleuserStatus)
+//         }
+//     };
+
+//     const users: User[] = allUsersData?.data || [];
+
+//     // Function to show more users
+//     const handleShowMore = () => {
+//         setVisibleUsersCount((prevCount) => prevCount + 20);
+//     };
+
+//     return (
+//         <div className="min-h-screen bg-gradient-to-br from-blue-50 to-gray-100 p-6">
+//             <div className="max-w-6xl mx-auto">
+//                 <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">
+//                     Users Management
+//                 </h1>
+//                 <div className="bg-white shadow-lg rounded-lg p-6">
+//                     <div className="text-gray-800 font-semibold mb-8 text-xl text-center">
+//                         Recommended Users
+//                     </div>
+//                     <div className="space-y-4">
+//                         {users.slice(0, visibleUsersCount).map((user: User) => (
+//                             <div
+//                                 key={user.id}
+//                                 className="flex items-center justify-between p-4 bg-gray-50 rounded-lg shadow-md hover:shadow-lg transition-shadow"
+//                             >
+//                                 <div className="flex items-center space-x-4">
+//                                     {user?.profileImage ? (
+//                                         <Image
+//                                             width={50}
+//                                             height={50}
+//                                             src={user?.profileImage}
+//                                             alt={user.firstName}
+//                                             className="w-14 h-14 rounded-full object-cover"
+//                                         />
+//                                     ) : (
+//                                         <div className="w-14 h-14 rounded-full bg-blue-500 text-white flex justify-center items-center font-bold text-lg">
+//                                             {user?.firstName.slice(0, 1).toUpperCase()}
+//                                         </div>
+//                                     )}
+//                                     <span className="text-lg font-bold text-gray-700">
+//                                         {`${user.firstName} ${user.lastName}`}
+//                                     </span>
+//                                 </div>
+//                                 <div className="flex space-x-2">
+//                                     <button
+//                                         className="px-4 py-2 bg-blue-500 text-white font-medium rounded-lg shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+//                                         onClick={() => handleAddRecommended(user.id)}
+//                                     >
+//                                         Remove from Recommended user
+//                                     </button>
+//                                     <button
+//                                         className="px-4 py-2 bg-red-500 text-white font-medium rounded-lg shadow hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+//                                         onClick={() => handleBlockUser(user.id)}
+//                                     >
+//                                         Block User
+//                                     </button>
+//                                 </div>
+//                             </div>
+//                         ))}
+//                     </div>
+//                     {visibleUsersCount < users.length && (
+//                         <div className="text-center mt-6">
+//                             <button
+//                                 className="px-6 py-3 bg-gray-800 text-white font-medium rounded-lg shadow-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-600"
+//                                 onClick={handleShowMore}
+//                             >
+//                                 Show More
+//                             </button>
+//                         </div>
+//                     )}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default RecommendedUserPage;
+
+
+
+
+
+
+
+
+
+
+"use client";
+import Image from "next/image";
+import React, { useMemo, useState } from "react";
+import { User } from "../layout";
+import {
+    useGetAllRecommendedUsersQuery,
+    useToggleBlockedMutation,
+    useToggleVerifiedAndRecommendedMutation,
+} from "@/redux/features/api/authApi";
+import Loading from "@/components/shared/Loading";
+import toast from "react-hot-toast";
+import { debounce } from "lodash";
 
 const RecommendedUserPage = () => {
     const { data: allUsersData, isLoading: allUsersDataLoading } = useGetAllRecommendedUsersQuery("");
-    const [ toggleVerifiedAndRecommended, { isLoading: toggleVerifiedAndRecommendedLoading } ] = useToggleVerifiedAndRecommendedMutation();
+    const [ toggleVerifiedAndRecommended, { isLoading: toggleVerifiedAndRecommendedLoading } ] =
+        useToggleVerifiedAndRecommendedMutation();
     const [ toggleBlockedMutation, { isLoading: toggleBlockedLoading } ] = useToggleBlockedMutation();
     const [ visibleUsersCount, setVisibleUsersCount ] = useState(20);
+    const [ searchTerm, setSearchTerm ] = useState("");
+    const [ filteredUsers, setFilteredUsers ] = useState<User[]>([]);
+
+
+    const users: User[] = useMemo(() => allUsersData?.data || [], [ allUsersData?.data ])
+
+    // Initialize filtered users when users data is fetched
+    React.useEffect(() => {
+        setFilteredUsers(users);
+    }, [ users ]);
 
     if (allUsersDataLoading) {
         return <Loading />;
     }
 
+
+    // Debounced search function
+    const debouncedSearch = debounce((term: string) => {
+        const filtered = users.filter((user) =>
+            `${user.firstName} ${user.lastName}`.toLowerCase().includes(term.toLowerCase())
+        );
+        setFilteredUsers(filtered);
+    }, 300);
+
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setSearchTerm(value);
+        debouncedSearch(value);
+    };
+
     const handleAddRecommended = async (userId: string) => {
-        const toggleuserStatus = toast.loading("updating user status.")
+        const toggleuserStatus = toast.loading("Updating user status...");
         try {
             const response = await toggleVerifiedAndRecommended({ id: userId });
             if (response.error) {
@@ -27,14 +194,14 @@ const RecommendedUserPage = () => {
             }
             toast.success("User status updated successfully!");
         } catch (error) {
-            toast.error("error while updating user status!")
+            toast.error("Error while updating user status!");
         } finally {
-            toast.dismiss(toggleuserStatus)
+            toast.dismiss(toggleuserStatus);
         }
     };
 
     const handleBlockUser = async (userId: string) => {
-        const toggleuserStatus = toast.loading("updating user status.")
+        const toggleuserStatus = toast.loading("Updating user status...");
         try {
             const response = await toggleBlockedMutation({ id: userId });
             if (response.error) {
@@ -43,13 +210,11 @@ const RecommendedUserPage = () => {
             }
             toast.success("User status updated successfully!");
         } catch (error) {
-            toast.error("error while updating user status!")
+            toast.error("Error while updating user status!");
         } finally {
-            toast.dismiss(toggleuserStatus)
+            toast.dismiss(toggleuserStatus);
         }
     };
-
-    const users: User[] = allUsersData?.data || [];
 
     // Function to show more users
     const handleShowMore = () => {
@@ -59,15 +224,24 @@ const RecommendedUserPage = () => {
     return (
         <div className="min-h-screen bg-gradient-to-br from-blue-50 to-gray-100 p-6">
             <div className="max-w-6xl mx-auto">
-                <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">
-                    Users Management
-                </h1>
+                <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">Users Management</h1>
                 <div className="bg-white shadow-lg rounded-lg p-6">
-                    <div className="text-gray-800 font-semibold mb-8 text-xl text-center">
-                        Recommended Users
+                    <div className="text-gray-800 font-semibold mb-8 text-xl text-center">Recommended Users</div>
+
+                    {/* Search Bar */}
+                    <div className="relative mb-6  text-black">
+                        <input
+                            type="text"
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                            placeholder="Search users by name..."
+                            className="w-full px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 border-2 font-bold border-cyan-500"
+                        />
                     </div>
+
+                    {/* User List */}
                     <div className="space-y-4">
-                        {users.slice(0, visibleUsersCount).map((user: User) => (
+                        {filteredUsers.slice(0, visibleUsersCount).map((user: User) => (
                             <div
                                 key={user.id}
                                 className="flex items-center justify-between p-4 bg-gray-50 rounded-lg shadow-md hover:shadow-lg transition-shadow"
@@ -86,18 +260,18 @@ const RecommendedUserPage = () => {
                                             {user?.firstName.slice(0, 1).toUpperCase()}
                                         </div>
                                     )}
-                                    <span className="text-lg font-bold text-gray-700">
-                                        {`${user.firstName} ${user.lastName}`}
-                                    </span>
+                                    <span className="text-lg font-bold text-gray-700">{`${user.firstName} ${user.lastName}`}</span>
                                 </div>
                                 <div className="flex space-x-2">
                                     <button
+                                        disabled={toggleBlockedLoading || toggleVerifiedAndRecommendedLoading}
                                         className="px-4 py-2 bg-blue-500 text-white font-medium rounded-lg shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
                                         onClick={() => handleAddRecommended(user.id)}
                                     >
-                                        Remove from Recommended user
+                                        Remove from Recommended User
                                     </button>
                                     <button
+                                        disabled={toggleBlockedLoading || toggleVerifiedAndRecommendedLoading}
                                         className="px-4 py-2 bg-red-500 text-white font-medium rounded-lg shadow hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
                                         onClick={() => handleBlockUser(user.id)}
                                     >
@@ -107,7 +281,9 @@ const RecommendedUserPage = () => {
                             </div>
                         ))}
                     </div>
-                    {visibleUsersCount < users.length && (
+
+                    {/* Show More Button */}
+                    {visibleUsersCount < filteredUsers.length && (
                         <div className="text-center mt-6">
                             <button
                                 className="px-6 py-3 bg-gray-800 text-white font-medium rounded-lg shadow-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-600"
